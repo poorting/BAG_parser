93953386f83a0a1ddc82cd859aa7810d69537780
poorting <remco.poortinga@surf.nl>
2025-07-26 12:17:21 +0200

    Added ability to export to .duckdb

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -22,10 +22,16 @@ class Exporter:
             utils.print_log("DuckDB database bevat geen adressen tabel. Importeer BAG eerst.", True)
             quit()
 
-        sqlcmd = f"COPY ({sql}) TO '{output_filename}' {export_options};"
-        self.database.connection.execute(sqlcmd)
+        if not output_filename.endswith('.duckdb'):
+            sqlcmd = f"COPY ({sql}) TO '{output_filename}' {export_options};"
+            self.database.connection.execute(sqlcmd)
+        else:
+            self.database.connection.execute(f"ATTACH '{output_filename}' AS export;")
+            sqlcmd = f"CREATE OR REPLACE TABLE export.{export_options} AS {sql}"
+            self.database.connection.execute(sqlcmd)
+            self.database.connection.execute(f"DETACH export;")
 
-    def export(self, output_filename, export_options, export_geometry=False):
+    def _lon_lat_export(self, output_filename, export_geometry=False):
         exp_geom = ""
         exp_lon_lat = ""
         if output_filename.endswith('.parquet'):
@@ -37,7 +43,29 @@ class Exporter:
         elif output_filename.endswith('.tsv'):
             exp_lon_lat = "st_astext(a.lon_lat) AS lon_lat,"
             exp_geom = "st_astext(a.geometry) as geometry " if export_geometry else ""
+        elif output_filename.endswith('.duckdb'):
+            exp_lon_lat = "a.lon_lat AS lon_lat,"
+            exp_geom = "a.geometry as geometry " if export_geometry else ""
 
+        return exp_geom, exp_lon_lat
+
+
+    def export(self, output_filename, export_options, export_geometry=False):
+        # exp_geom = ""
+        # exp_lon_lat = ""
+        # if output_filename.endswith('.parquet'):
+        #     exp_lon_lat = "a.lon_lat AS lon_lat,"
+        #     exp_geom = "a.geometry AS geometry" if export_geometry else ""
+        # elif output_filename.endswith('.json'):
+        #     exp_lon_lat = "st_asgeojson(a.lon_lat) AS lon_lat,"
+        #     exp_geom = "st_asgeojson(a.geometry) as geometry " if export_geometry else ""
+        # elif output_filename.endswith('.tsv'):
+        #     exp_lon_lat = "st_astext(a.lon_lat) AS lon_lat,"
+        #     exp_geom = "st_astext(a.geometry) as geometry " if export_geometry else ""
+        # elif output_filename.endswith('.duckdb'):
+        #     exp_lon_lat = "a.lon_lat AS lon_lat,"
+        #     exp_geom = "a.geometry as geometry " if export_geometry else ""
+        exp_geom, exp_lon_lat = self._lon_lat_export(output_filename, export_geometry)
 
         sql = f"""
                 SELECT
@@ -64,12 +92,13 @@ class Exporter:
                   LEFT JOIN openbare_ruimten o ON a.openbare_ruimte_id = o.id
                   LEFT JOIN gemeenten g        ON a.gemeente_id        = g.id
                   LEFT JOIN woonplaatsen w     ON a.woonplaats_id      = w.woonplaats_id
-                  LEFT JOIN provincies p       ON g.provincie_id       = p.id"""
+                  LEFT JOIN provincies p       ON g.provincie_id       = p.id
+        """
 
         self.__export(output_filename, export_options, sql)
 
     def export_postcode(self, output_filename, export_options, is_parquet=False):
-        exp_parquet = "a.lon_lat AS lon_lat," if is_parquet else ""
+        exp_geom, exp_lon_lat = self._lon_lat_export(output_filename)
         sql = f"""
             SELECT
               o.naam                       AS straat,
@@ -78,55 +107,67 @@ class Exporter:
               a.postcode,
               a.latitude,
               a.longitude,
-              {exp_parquet}
+              {exp_lon_lat}
               w.naam                       AS woonplaats
             FROM adressen a
               LEFT JOIN openbare_ruimten o ON a.openbare_ruimte_id = o.id
-              LEFT JOIN woonplaatsen w     ON a.woonplaats_id      = w.woonplaats_id"""
+              LEFT JOIN woonplaatsen w     ON a.woonplaats_id      = w.woonplaats_id
+        """
 
         self.__export(output_filename, export_options, sql)
 
     def export_postcode4_stats(self, output_filename, export_options):
-        sql = """
+        exp_geom, exp_lon_lat = self._lon_lat_export(output_filename)
+        exp_lon_lat = exp_lon_lat.replace("a.lon_lat", "ST_Centroid(ST_Collect(list(a.lon_lat)))")
+        sql = f"""
           SELECT
             SUBSTR(a.postcode, 0, 5) AS pc4,
             AVG(a.latitude)          AS center_lat,
             AVG(a.longitude)         AS center_lon,
+            {exp_lon_lat}
             COUNT(1)                 AS aantal_adressen,
             FIRST(w.naam)            AS woonplaats
           FROM adressen a
             LEFT JOIN woonplaatsen w ON a.woonplaats_id = w.woonplaats_id
           WHERE a.postcode <> ''
-          GROUP BY pc4"""
-
+          GROUP BY pc4
+        """
         self.__export(output_filename, export_options, sql)
 
     def export_postcode5_stats(self, output_filename, export_options):
-        sql = """
+        exp_geom, exp_lon_lat = self._lon_lat_export(output_filename)
+        exp_lon_lat = exp_lon_lat.replace("a.lon_lat", "ST_Centroid(ST_Collect(list(a.lon_lat)))")
+        sql = f"""
           SELECT
             SUBSTR(a.postcode, 0, 6) AS pc5,
             AVG(a.latitude)          AS center_lat,
             AVG(a.longitude)         AS center_lon,
+            {exp_lon_lat}
             COUNT(1)                 AS aantal_adressen,
             FIRST(w.naam)            AS woonplaats
           FROM adressen a
             LEFT JOIN woonplaatsen w ON a.woonplaats_id = w.woonplaats_id
           WHERE a.postcode <> ''
-          GROUP BY pc5"""
+          GROUP BY pc5
+        """
 
         self.__export(output_filename, export_options, sql)
 
     def export_postcode6_stats(self, output_filename, export_options):
-        sql = """
+        exp_geom, exp_lon_lat = self._lon_lat_export(output_filename)
+        exp_lon_lat = exp_lon_lat.replace("a.lon_lat", "ST_Centroid(ST_Collect(list(a.lon_lat)))")
+        sql = f"""
           SELECT
             a.postcode       AS pc6,
             AVG(a.latitude)  AS center_lat,
             AVG(a.longitude) AS center_lon,
+            {exp_lon_lat}
             COUNT(1)         AS aantal_adressen,
             FIRST(w.naam)    AS woonplaats
           FROM adressen a
             LEFT JOIN woonplaatsen w ON a.woonplaats_id = w.woonplaats_id
           WHERE a.postcode <> ''
-          GROUP BY pc6"""
+          GROUP BY pc6
+        """
 
         self.__export(output_filename, export_options, sql)
